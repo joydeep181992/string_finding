b2633b6b6d9cf81b60945999befecec01ddac4a6
Joydeep Roy Chowdhury <32680197+joydeep181992@users.noreply.github.com>
2017-11-26 14:04:54 +0000

    Update alphabetic.py

--- alphabetic.py
+++ alphabetic.py
@@ -1,9 +1,12 @@
 # given string s.
 s = 'abcdrhgirgabcvjdenjjklfeffgh'
+
+#keyfunc function takes an arg which is passed in enumerate fun as tuple 
 def keyfunc(t):
     i, c = t
     return ord(c) - i
 a = []
+
 for k, v in groupby(enumerate(s), key=keyfunc):
     seq = [t[1] for t in v]
     if len(seq) > 1:
